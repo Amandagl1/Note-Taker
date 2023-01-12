47703c5fa004a149ab568d80536ac287013e03a6
Amanda Lee <leeandama87@gmail.com>
2023-01-11 22:51:43 -0800

    Put backdb file.

--- Develop/server.js
+++ Develop/server.js
@@ -14,11 +14,13 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/notes', (req, res) =>
+app.get('/api/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-
+app.get("/api/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, "db.json"))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
