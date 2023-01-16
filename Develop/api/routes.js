8d097c4eb18f0802aec72071b8f791c78c199f70
Amanda Lee <leeandama87@gmail.com>
2023-01-16 11:38:16 -0800

    Added delete routes and fixed post route.

--- Develop/api/routes.js
+++ Develop/api/routes.js
@@ -1,65 +1,54 @@
-const uuid = require('./helpers/uuid');
-const db = require('./db/db');
+const uuid = require('../helpers/uuid');
 const fs = require('fs');
+const router = require('express').Router();
 
-
-fs.readFileSync('db/db.json','utf8', (error, notes) => {
-
-  if (error) throw err;
-  let data = JSON.parse(notes);
-
-}).then((notes) => {
-  return res.json((notes));
-
-}).catch((err) => {
-  res.status(500).json(err);
-
-});
 // GET request for notes
-app.get('/api/notes', (req, res) => {
-  res.status(200).json(notes);
-  res.json(`${req.method} request received to get note`);
+router.get('/api/notes', (req, res) => {
+  // res.json(`${req.method} request received to get note`);
+  let notes = fs.readFileSync('db/db.json', "utf8");
+  res.status(200).json(JSON.parse(notes));
 
-  let notes = fs.readFileSync(db, "utf8");
-
-  res.json(JSON.parse(notes));
 
 });
+
+// router.get('/api/notes/:id', (req, res) => {
+//   let notes = fs.readFileSync('db/db.json', "utf8");
+
+//   const allNotes = JSON.parse(notes);
+//   const note = allNotes.find((n) => n.id === req.params.id);
+//   res.status(200).json(note);
+
+// });
   
 // POST request to add a note
-app.post('/api/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
 // Log that a POST request was received
   console.info(`${req.method} request received to add a new note`);
   
   const { title, text } = req.body;
-  
+  console.log(title, text);
   // If all the required properties are present
   if (title && text) {
   // Variable for the object to be saved
    const newNote = {
       title,
       text,
-      note_id: uuid(),
+      id: uuid(),
     };
   
    const response = {
       status: 'success',
-      body: newNote,
     };
 
     // Read file from db.json
-    let notes = fs.readFileSync(db, "utf8");
-
+    let notes = fs.readFileSync('db/db.json', "utf8");
     const parsedData = JSON.parse(notes);
     
-    parsedData.push(newNote);
+    let noteArray = [...parsedData, newNote]
+    console.log(noteArray);
 
-
-    // Convert the data to a string to save the note
-    const noteString = JSON.stringify(newNote);
-  
     // Writing the string to a file
-    fs.writeFile(`./db/${newNote.title}.json`, noteString, (err) =>
+    fs.writeFile(`db/db.json`, JSON.stringify(noteArray), (err) =>
       err
         ? console.error(err)
         : console.log(
@@ -76,3 +65,31 @@ app.post('/api/notes', (req, res) => {
 
   });
 
+  router.delete('/api/notes/:id', (req, res) => {
+    try {
+    let notes = fs.readFileSync('db/db.json', "utf8");
+    const parsedData = JSON.parse(notes);
+    var newArray = parsedData.filter(note => note.id !== req.params.id)
+    console.log(newArray);
+
+    fs.writeFile(`db/db.json`, JSON.stringify(newArray), (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+          `Note for ${req.params.id} has been deleted from the JSON file`
+          )
+    );
+
+    const response = {
+      status: 'success',
+    };
+
+    res.json(response)
+  } catch(err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+
+  }); 
+
+  module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,15 @@
 // Requiring in dependencies
 const express = require('express');
 const path = require('path');
-const dbFile = require('./db/db.json')
-
+const routes = require('./api/routes')
 const PORT = 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.use('/', routes)
 
 // Getting the homepage
 app.get('/', (req, res) => {
@@ -20,9 +21,6 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
-app.get('/api/notes', (req, res) => {
-  res.json(dbFile);
-});
 
 
 app.listen(PORT, () =>
